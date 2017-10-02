8dcbefd4eaf86c0b4bb54a4abcd813145c0e2c84
CesarJs <csr2314@gmail.com>
2017-10-02 11:15:40 -0300

    crud feito

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,224 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.simulate')
 
-        <title>Laravel</title>
+@section ('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<div class="flex-center position-ref full-height">
+    <div class="content">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <form action="/buscar" method="GET" class="form">
+            <div class="col-md-6 col-md-offset-3">
+                <form id="frmSimulador" action="/planos.aspx" method="post">
+                    <div class="simulador-box">
+                        <div class="contents">
+                            <h3>Simulador Online</h3>
+                            <div class="tipo row">
+                                <div class="item col-xs-3 text-center" id="dvTipoImoveis" style="">
+                                    <input name="tipo" id="tipoImoveis" value="1" checked="checked" type="radio">
+                                    <label class="btn btn-primary"  onClick="escolhaCategory((this).id);" for="tipoImoveis" id="lblTipoImovel">
+                                        <i class="fa fa-home"></i>
+                                        IMÓVEIS
+                                    </label>
+                                </div>
+                                <div class="item col-xs-3 text-center" id="dvTipoVeiculos" style="">
+                                    <input name="tipo" id="tipoVeiculos" value="2" type="radio">
+                                    <label class="btn btn-default"  onClick="escolhaCategory((this).id);" for="tipoVeiculos" id="lblTipoVeiculo">
+                                        <i class="fa fa-car"></i>
+                                        VEÍCULOS
+                                    </label>
+                                </div>
+                                <div class="item col-xs-3 text-center" id="dvTipoServicos" style="">
+                                    <input name="tipo" id="tipoServicos" value="3" type="radio">
+                                    <label class="btn btn-default" onClick="escolhaCategory((this).id);"  for="tipoServicos" id="lblTipoServico">
+                                        <i class="fa fa-wrench"></i>
+                                        SERVIÇOS
+                                    </label>
+                                </div>
+                                <div class="item col-xs-3 text-center" id="dvTipoEletro" style="">
+                                    <input name="tipo" id="tipoEletro" value="4" type="radio">
+                                    <label class="btn btn-default" onClick="escolhaCategory((this).id);"  for="tipoEletro" id="lblTipoEletro">
+                                        <i class="fa fa-coffe"></i>
+                                        ELETRO
+                                    </label>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                            <br>
+                            <div class="row">
+                            <p>SIMULAR PLANO POR VALOR</p>
+                                <div class="col-xs-6 text-center">
+                                    <input id="credito" name="keyTipo" onFocus="$('#optionKey').html('DO CRÉDITO');"  value="credito" checked="checked" type="radio">
+                                    <label class="btn btn-primary" id="lbl-credito" onclick="escolhaKey('credito');" for="credito">
+                                        CRÉDITO
+                                    </label>
+                                </div>
+                                <div class="col-xs-6 text-center">
+                                    <input id="parcela" name="keyTipo" onFocus="$('#optionKey').html(' DA PARCELA');" value="parcela" type="radio">
+                                    <label class="btn btn-default" id="lbl-parcela" onclick="escolhaKey('parcela');"  for="parcela">
+                                        PARCELA
+                                    </label>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+                            <div class="row">
+                                <p>ARRASTE E ESCOLHA O VALOR <span id="optionKey">DO CRÉDITO</span></p>
+                                <div class="row">
+                                    
 
-            .position-ref {
-                position: relative;
-            }
+                                </div>
+                                <div id="slider" class="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content"><div class="ui-slider-range ui-corner-all ui-widget-header" style="left: 0%; width: 100%;"></div><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 0%;"></span><span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: 100%;"></span></div>
+                            </div>
+                            <br>
+                            <div id="row">
+                                <div class="form uppercase">
+                                    <div class="alert alert-danger" id="dvErro" style="display: none;">
+                                        <div class="aviso">Mensagem</div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <label>Informe seu nome</label>
+                                                <input class="form-control" id="txtNome" maxlength="64" name="inputNome" placeholder="Nome" type="text">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <div class="form-group">
+                                                <label>E-mail</label>
+                                                <input class="form-control" name="inputEmail" maxlength="64" id="txtEmail" placeholder="Email" type="email">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label>DDD + Telefone</label>
+                                                <input class="form-control" name="inputTelefone" maxlength="14" id="txtTel" placeholder="Telefone" onkeypress="Mascara(this);" type="text">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <label>CEP</label>
+                                                <input class="form-control" name="inputCEP" maxlength="8" id="txtCep" placeholder="CEP" onkeypress="return SomenteNumber(event)" type="text">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <input name="inputContato" id="chkReceberContato" checked="checked" type="checkbox">
+                                            <label>Quero solicitar contato de um consultor</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div align="center">
+                                <button class="btn btn-default" id="btnSimular">Simular Agora</button>
+                            </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </div>
-    </body>
-</html>
+        </form>
+
+    </div>
+</div>
+@endsection
+@section('post-script')
+
+<script type="text/javascript">
+function escolhaCategory(tipo){
+    switch (tipo) {
+        case 'lblTipoImovel':
+
+            $('#lblTipoImovel').removeClass('btn-default');
+            $('#lblTipoImovel').addClass('btn-primary');
+
+            $('#lblTipoVeiculo').removeClass('btn-primary');
+            $('#lblTipoVeiculo').addClass('btn-default');
+
+            $('#lblTipoServico').removeClass('btn-primary');
+            $('#lblTipoServico').addClass('btn-default');
+
+            $('#lblTipoEletro').removeClass('btn-primary');
+            $('#lblTipoEletro').addClass('btn-default');
+
+            // statements_1
+            break;
+        case 'lblTipoVeiculo':
+            // statements_1
+            $('#lblTipoVeiculo').removeClass('btn-default');
+            $('#lblTipoVeiculo').addClass('btn-primary');
+
+            $('#lblTipoImovel').removeClass('btn-primary');
+            $('#lblTipoImovel').addClass('btn-default');
+
+            $('#lblTipoServico').removeClass('btn-primary');
+            $('#lblTipoServico').addClass('btn-default');
+
+            $('#lblTipoEletro').removeClass('btn-primary');
+            $('#lblTipoEletro').addClass('btn-default');
+            break;
+        case 'lblTipoServico':
+            // statements_1
+            $('#lblTipoServico').removeClass('btn-default');
+            $('#lblTipoServico').addClass('btn-primary');
+
+            $('#lblTipoImovel').removeClass('btn-primary');
+            $('#lblTipoImovel').addClass('btn-default');
+
+            $('#lblTipoVeiculo').removeClass('btn-primary');
+            $('#lblTipoVeiculo').addClass('btn-default');
+
+            $('#lblTipoEletro').removeClass('btn-primary');
+            $('#lblTipoEletro').addClass('btn-default');
+            break;
+        case 'lblTipoEletro':
+            // statements_1
+            $('#lblTipoEletro').removeClass('btn-default');
+            $('#lblTipoEletro').addClass('btn-primary');
+
+            $('#lblTipoImovel').removeClass('btn-primary');
+            $('#lblTipoImovel').addClass('btn-default');
+
+            $('#lblTipoVeiculo').removeClass('btn-primary');
+            $('#lblTipoVeiculo').addClass('btn-default');
+
+            $('#lblTipoServico').removeClass('btn-primary');
+            $('#lblTipoServico').addClass('btn-default');
+            break;
+        default:
+            // statements_def
+            break;
+    }
+}
+function escolhaKey(tipo){
+    switch (tipo) {
+        case 'credito':
+            $('#lbl-credito').removeClass('btn-default');
+            $('#lbl-credito').addClass('btn-primary');
+
+            $('#lbl-parcela').removeClass('btn-primary');
+            $('#lbl-parcela').addClass('btn-default');
+            // statements_1
+            break;
+        case 'parcela':
+
+            $('#lbl-credito').removeClass('btn-primary');
+            $('#lbl-credito').addClass('btn-default');
+
+            $('#lbl-parcela').removeClass('btn-default');
+            $('#lbl-parcela').addClass('btn-primary');
+            // statements_1
+            break;
+        default:
+            // statements_def
+            break;
+    }
+}
+
+</script>
+@endsection
